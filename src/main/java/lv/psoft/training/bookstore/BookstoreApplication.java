7294642cc690912800ee5b71b3e98535eb08638b
OlegMiroshnikov <oleg.psoft@gmail.com>
2021-05-01 18:38:21 +0300

    initial commit

---
+++ src/main/java/lv/psoft/training/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package lv.psoft.training.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
+
+}
