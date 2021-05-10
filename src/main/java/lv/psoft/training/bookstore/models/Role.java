dadd0202215149f64621efaaf149d10fbb33e735
OlegMiroshnikov <oleg.psoft@gmail.com>
2021-05-10 16:15:53 +0300

    add @JsonIdentityReference and @JsonIdentityInfo annotations to serialize collections of these objects as IDs

--- src/main/java/lv/psoft/training/bookstore/models/Role.java
+++ src/main/java/lv/psoft/training/bookstore/models/Role.java
@@ -1,5 +1,7 @@
 package lv.psoft.training.bookstore.models;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
 import lombok.Builder;
 import org.springframework.data.annotation.Id;
@@ -8,6 +10,9 @@ import org.springframework.data.redis.core.index.Indexed;
 
 @Data
 @Builder
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id")
 @RedisHash
 public class Role {
    @Id
